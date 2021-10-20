408ae6762c09ef56c69d51d18ee74bdd786dbeda
kerenhappuch441 <91006012+kerenhappuch441@users.noreply.github.com>
2021-10-20 17:13:57 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -21,13 +21,13 @@ TEST_CASE("average is NaN for empty array") {
   /*   printf("2nd test cast %d",computedStats.average);
      printf("2nd test cast %d",computedStats.max);
      printf("2nd test cast %d",computedStats.min);*/
-   /* REQUIRE(isnan(computedStats.average)==1);*/
+    REQUIRE(<std::isnan(computedStats.average)==1);
    
    std::cout<<"the value is "<<std::isnan(computedStats.max);
    std::cout<<"the value is "<<std::isnan(computedStats.min);
    std::cout<<"the value is "<<std::isnan(computedStats.average);
- /*  REQUIRE(isnan(computedStats.max)==1);
-    REQUIRE(isnan(computedStats.min)==1);*/
+    REQUIRE(<std::isnan(computedStats.max)==1);
+    REQUIRE(<std::isnan(computedStats.min)==1);
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
