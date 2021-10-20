dc2055a6fb2abf5c911517a405b1056369005fa5
kerenhappuch441 <91006012+kerenhappuch441@users.noreply.github.com>
2021-10-20 14:48:27 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,19 @@
 #include "stats.h"
 using namespace Statistics;
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& vectorarray ) {
     //Implement statistics here
+   Stats stat; 
+  if(vectorarray.empty())
+  {      
+    stat.min=NAN ;
+    stat.max=NAN;
+    stat.avg=NAN; 
+  }
+  else
+  {
+     stat.min = *min_element(vectorarray.begin(), vectorarray.end());
+     stat.max = *max_element(vectorarray.begin(), vectorarray.end());
+     stat.avg = avg1(vectorarray);     
+  } 
+     return stat; 
 }
